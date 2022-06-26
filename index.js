1b5223dc07f7f95b8bb96a93d8959731f24638d2
Luke McDougall <lmcdougall@unm.edu>
2022-06-25 23:59:52 -0600

    init github repo

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const client = new Discord.Client({
+     intents: [
+       "GUILDS",
+       "GUILD_MESSAGES"
+     ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "mango") {
+        message.reply("is GAY!")
+    }
+})
+
+
+client.login(process.env.TOKEN)
